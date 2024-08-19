b81937cb95e6fe3a71242b3eadb523a61f3147d2
okovalen <kovalenko.oleg@icloud.com>
2024-08-19 13:33:40 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -4,8 +4,16 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
                 or recipient.endswith('.net')
                 or recipient.endswith('@')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
+    elif not (sender.endswith('.com')
+                or sender.endswith('.ru')
+                or sender.endswith('.net')
+                or sender.endswith('@')):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
     elif sender == recipient:
         print('Нельзя отправить письмо самому себе')
+
     else:
         if sender == 'university.help@gmail.com':
             print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
